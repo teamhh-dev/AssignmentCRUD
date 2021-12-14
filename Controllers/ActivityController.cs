3127ec146060a2c39209b03a96f669837e63153b
Hamza Ali <teamhh.dev@gmail.com>
2021-12-14 21:01:27 +0500

    ClassAssignment

---
+++ Controllers/ActivityController.cs
@@ -0,0 +1,26 @@
+﻿using AssignmentCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AssignmentCRUD.Controllers
+{
+    public class ActivityController : Controller
+    {
+        // GET: Activity
+        public ActionResult Index()
+        {
+            List<Activity> activities = new List<Activity>() { new Activity() { ID=1,Name="Cinema",Duration="3 hours",Amount=50}, new Activity() { ID = 2, Name = "Hiking", Duration = "5 hours", Amount = 100 },new Activity() { ID = 3, Name = "Chalet", Duration = "4 hours", Amount = 400 } };
+
+            return View(activities);
+        }
+        public ActionResult Vote(int id)
+        {
+            //List<Activity> activities = new List<Activity>() { new Activity() { ID=1,Name="Cinema",Duration="3 hours",Amount=50}, new Activity() { ID = 2, Name = "Hiking", Duration = "5 hours", Amount = 100 },new Activity() { ID = 3, Name = "Chalet", Duration = "4 hours", Amount = 400 } };
+
+            return View();
+        }
+    }
+}
---
+++ Models/Vote.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AssignmentCRUD.Models
+{
+    public class Vote
+    {
+        public int ActivityID { get; set; }
+        public string VoterName { get; set; }
+        public int Radius { get; set; }
+    }
+}
